fe462fd8a3f527227fed20cc49bb8291ce41fc92
Paola987q <131223420+Paola987q@users.noreply.github.com>
2025-12-26 14:36:47 -0500

    Update firebaseConfig.js

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -1,1 +1,21 @@
-------
+// firebase/firebaseConfig.js
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+
+// Tu configuración de Firebase
+const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { auth, db };
+
