9a6dd20c53c3f4364022e2fa26cf7cdc9d70da66
Paola987q <131223420+Paola987q@users.noreply.github.com>
2025-12-26 15:24:49 -0500

    Update firebaseConfig.js

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -5,12 +5,7 @@ import { getFirestore } from 'firebase/firestore';
 
 // Tu configuración de Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyDUh4mujHKfjVnmp7zoWVZC6Tz-4kjZfVI",
-  authDomain: "myschoolsystem-1e369.firebaseapp.com",
-  projectId: "myschoolsystem-1e369",
-  storageBucket: "myschoolsystem-1e369.firebasestorage.app",
-  messagingSenderId: "186890639159",
-  appId: "1:186890639159:web:33b061882b22ecc426ecb2"
+ 
 };
 
 const app = initializeApp(firebaseConfig);
@@ -18,3 +13,4 @@ const auth = getAuth(app);
 const db = getFirestore(app);
 
 export { auth, db };
+
