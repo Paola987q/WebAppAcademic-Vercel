9b05578f04f28a8b2e0e22901a98901f3ca595ff
Paola987q <131223420+Paola987q@users.noreply.github.com>
2025-12-26 15:41:55 -0500

    Update firebaseConfig.js

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -1,16 +1,23 @@
 // firebase/firebaseConfig.js
-import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
-// Tu configuración de Firebase
+// Configuración de Firebase 
 const firebaseConfig = {
- 
+  apiKey: import.meta.env.VIT_FB_API_KEY,
+  authDomain: import.meta.env.VIT_FB_AUTH_DOMAIN,
+  projectId: import.meta.env.VIT_FB_PROJECT_ID,
+  storageBucket: import.meta.env.VIT_FB_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VIT_FB_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VIT_FB_APP_ID,
 };
 
+// Inicializar Firebase
 const app = initializeApp(firebaseConfig);
+
+// Servicios
 const auth = getAuth(app);
 const db = getFirestore(app);
 
 export { auth, db };
-
